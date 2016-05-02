8d97b5c679585ab942753b53fa0b8ac1174b93c6
Danny Bowman <danny@d-bow.com>
2016-05-02 19:26:41 -0400

    Fix nav linebreaks

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -7,11 +7,15 @@ import {Link} from 'react-router';
 export default () => (
   <nav className={css.nav}>
     <h2 className={css.h2}>Danny Bowman</h2>&#8217;s&nbsp;
-    <Link className={css.link} to="/work" activeClassName={css.active}>work</Link>,&nbsp;
-    <Link className={css.link} to="/writing" activeClassName={css.active}>writing</Link>,&nbsp;
-    <Link className={css.link} to="/music" activeClassName={css.active}>music</Link>,&nbsp;
-    <Link className={css.link} to="/instapoetry" activeClassName={css.active}>insta-poems</Link>,&nbsp;
-    <span className={css.amp}>&amp;</span>&nbsp;
+    <Link className={css.link} to="/work" activeClassName={css.active}>work</Link>
+    <span>, </span>
+    <Link className={css.link} to="/writing" activeClassName={css.active}>writing</Link>
+    <span>, </span>
+    <Link className={css.link} to="/music" activeClassName={css.active}>music</Link>
+    <span>, </span>
+    <Link className={css.link} to="/instapoetry" activeClassName={css.active}>insta-poems</Link>
+    <span className={css.amp}> &amp;</span>
+    <span> </span>
     <Link className={css.link} to="/info" activeClassName={css.active}>info</Link>.
   </nav>
 );
