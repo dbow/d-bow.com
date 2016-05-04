6eabec97adad1ffa84088ef3e796d6831f310c94
Danny Bowman <danny@d-bow.com>
2016-05-04 16:06:28 -0400

    Adjust nav, post overflow, and font fallback

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -8,11 +8,11 @@ export default () => (
   <nav className={css.nav}>
     <h2 className={css.h2}>Danny Bowman</h2>&#8217;s&nbsp;
     <Link className={css.link} to="/work" activeClassName={css.active}>work</Link>
-    ,&nbsp;
+    <span>, </span>
     <Link className={css.link} to="/writing" activeClassName={css.active}>writing</Link>
-    ,&nbsp;
+    <span>, </span>
     <Link className={css.link} to="/music" activeClassName={css.active}>music</Link>
-    ,&nbsp;
+    <span>, </span>
     <Link className={css.link} to="/instapoetry" activeClassName={css.active}>insta-poems</Link>
     <span className={css.amp}> &amp; </span>
     <Link className={css.link} to="/info" activeClassName={css.active}>info</Link>.
