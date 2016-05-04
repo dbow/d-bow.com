e554a092a24ddffea1dbf84adbc1e3b2661cfc89
Danny Bowman <danny@d-bow.com>
2016-05-04 13:18:31 -0400

    Upgrade to React v15

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -8,14 +8,14 @@ export default () => (
   <nav className={css.nav}>
     <h2 className={css.h2}>Danny Bowman</h2>&#8217;s&nbsp;
     <Link className={css.link} to="/work" activeClassName={css.active}>work</Link>
-    <span>, </span>
+    ,&nbsp;
     <Link className={css.link} to="/writing" activeClassName={css.active}>writing</Link>
-    <span>, </span>
+    ,&nbsp;
     <Link className={css.link} to="/music" activeClassName={css.active}>music</Link>
-    <span>, </span>
+    ,&nbsp;
     <Link className={css.link} to="/instapoetry" activeClassName={css.active}>insta-poems</Link>
-    <span className={css.amp}> &amp;</span>
-    <span> </span>
+    ,&nbsp;
+    <span className={css.amp}> &amp; </span>
     <Link className={css.link} to="/info" activeClassName={css.active}>info</Link>.
   </nav>
 );
